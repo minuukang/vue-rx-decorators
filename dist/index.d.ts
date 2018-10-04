aa129ad87c262f919e9a1cf591c27c30f4027819
MinwooKang <kmw@designgj.com>
2018-10-04 11:03:09 +0900

    build 0.0.6

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,6 +3,10 @@ import { Observable } from "rxjs";
 export interface ObservableMethod extends Observable<any> {
     (...args: any[]): any;
 }
+export interface DOMStreamObservable<T extends Event> {
+    event: T;
+    data: any;
+}
 export declare function DOMStream(): VueDecorator;
 export declare function ObservableMethod(): VueDecorator;
 export declare function Subscription<T>(callback?: () => Observable<T>): VueDecorator;
